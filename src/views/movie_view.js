50b680f3b07963131ade350c6db01e7a22ded7db
Anthony Starke <starke@gmail.com>
2019-03-02 09:56:15 +0000

    fully working with some basic CSS

--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -7,27 +7,30 @@ MovieView.prototype.buildElement = function (element,content,parent) {
   const newItem = document.createElement(element);
   newItem.textContent = content;
   parent.appendChild(newItem);
+  // return newItem;
 };
 
-MovieView.prototype.buildItem = function (movieObject) {
+MovieView.prototype.buildItem = function (movieObject,className) {
 
   const container = document.createElement('div');
-  container.classList.add('movieItem');
+  container.classList.add(className);
   this.primaryParent.appendChild(container);
 
   this.buildElement('h2',movieObject.title,container);
   this.buildElement('p',`Score: ${movieObject.rt_score}`,container);
+  this.buildElement('p',`Release Year: ${movieObject.release_date}`,container);
   this.buildElement('p',movieObject.description,container);
+
 };
 
 MovieView.prototype.render = function (moviesData) {
 
   if(!Array.isArray(moviesData)){
-    this.buildItem(moviesData);
+    this.buildItem(moviesData,'movieItem');
   } else {
 
     moviesData.forEach((movie) => {
-      this.buildItem(movie);
+      this.buildItem(movie,'movieItems');
     })
   };
 };
